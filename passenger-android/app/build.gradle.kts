37040f2b68556221a0891fe2059e261dec1830d7
ArmandoCH85 <armando.correah@gmail.com>
2026-09-21 21:59:24 -0500

    chore(passenger): bump versionCode 20260920 -> 20260924, versionName 0.2.9 -> 0.2.10

--- passenger-android/app/build.gradle.kts
+++ passenger-android/app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.sitech.clinica.empleados"
         minSdk = 26
         targetSdk = 36
-        versionCode = 20260920
-        versionName = "0.2.9"
+        versionCode = 20260924
+        versionName = "0.2.10"
     }
 
     signingConfigs {
